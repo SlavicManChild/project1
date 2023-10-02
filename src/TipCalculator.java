d4a5ad3e7e7d1abb88a258e24d8f17bdf0fe8c96
Gregory <slavicservant@gmail.com>
2023-10-01 20:59:22 -0400

    third commit, shoudl ruin fine but no rounding yet

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -20,16 +20,17 @@ public class TipCalculator{
         scan.nextLine();
         System.out.println("What is the percent tip (1 - 100)");
         percentTip = scan.nextInt();
+        int originalPercentTip = percentTip;
         percentTip /= 100;
         scan.nextLine();
 
-        System.out.print("Please enter the cost of each individual item, to end and calculate please enter -1: ");
+        System.out.print("Enter the cost in dollars and cents:  ");
         System.out.println();
         temp =  scan.nextInt();
         while(temp != -1){
 
             totalCostBT += temp;
-            System.out.print("Please enter the cost of each individual item, to end and calculate please enter -1: ");
+            System.out.print("Enter the cost in dollars and cents:  ");
             System.out.println();
             temp =  scan.nextInt();
             scan.nextLine();
@@ -44,6 +45,11 @@ public class TipCalculator{
         System.out.println("..................................................");
         System.out.println("Total bill before tip: " + totalCostBT);
         System.out.println("Total percentage: " + percentTip);
+        System.out.println("Total tip: " + originalPercentTip);
+        System.out.println("Total bill with tip: " + totalCostAT);
+        System.out.println("Per person cost before tip: " + costPerPersonBT);
+        System.out.println("Tip per person: " + tipPerPerson);
+        System.out.println("Total cost per person" + totalCostAT);
 
 
 
