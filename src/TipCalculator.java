c7e71712ddcde8a6f9f677b2e49ee64910e55141
Gregory <slavicservant@gmail.com>
2023-10-01 20:54:00 -0400

    second commit

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -13,12 +13,14 @@ public class TipCalculator{
         int percentTip;
         double tipAmount;
         int peopleAmount;
-
+        double totalCostAT;
+        System.out.println("Welcome to the tip calculator!");
         System.out.println("How many people are im your group");
         peopleAmount = scan.nextInt();
         scan.nextLine();
         System.out.println("What is the percent tip (1 - 100)");
         percentTip = scan.nextInt();
+        percentTip /= 100;
         scan.nextLine();
 
         System.out.print("Please enter the cost of each individual item, to end and calculate please enter -1: ");
@@ -32,14 +34,16 @@ public class TipCalculator{
             temp =  scan.nextInt();
             scan.nextLine();
         }
+        tipAmount = totalCostBT * percentTip;
+        totalCostAT = totalCostBT + tipAmount;
+        double costPerPersonBT = totalCostBT / peopleAmount;
+        double costPerPersonAT = totalCostAT / peopleAmount;
+        double tipPerPerson = tipAmount / peopleAmount;
 
 
-
-
-
-
-
-        System.out.println("Welcome to the tip calculator! ");
+        System.out.println("..................................................");
+        System.out.println("Total bill before tip: " + totalCostBT);
+        System.out.println("Total percentage: " + percentTip);
 
 
 
